5723fc9c9235345005b7f143c054b1a172c7aba8
Artem Zakharchenko <artem.zakharchenko@actum.cz>
2017-05-30 20:21:07 +0200

    Create action: Use proper indentation

--- src/createAction.js
+++ src/createAction.js
@@ -1,14 +1,14 @@
 /* Set of default action creator options */
 const defaultOptions = {
-    /* Predefined action types */
-    types: {
-        request: 'REQUEST',
-        success: 'SUCCESS',
-        error: 'ERROR'
-    },
+  /* Predefined action types */
+  types: {
+    request: 'REQUEST',
+    success: 'SUCCESS',
+    error: 'ERROR'
+  },
 
-    /* Format function for each action type */
-    format: (name, type) => `${name}_${type}`
+  /* Format function for each action type */
+  format: (name, type) => `${name}_${type}`
 };
 
 /**
@@ -19,22 +19,22 @@ const defaultOptions = {
  * @return {Object}
  */
 export function createAction(actionName, customOptions) {
-    const options = Object.assign({}, defaultOptions, customOptions);
+  const options = Object.assign({}, defaultOptions, customOptions);
 
-    /* When custom format function is present, format the action types respectively */
-    if (options.format) {
-        const { types: actionTypes } = options;
-        const action = {};
+  /* When custom format function is present, format the action types respectively */
+  if (options.format) {
+    const { types: actionTypes } = options;
+    const action = {};
 
-        Object.keys(actionTypes).forEach((typeKey) => {
-            const typeValue = actionTypes[typeKey];
-            action[typeKey] = options.format(actionName, typeValue);
-        });
+    Object.keys(actionTypes).forEach((typeKey) => {
+      const typeValue = actionTypes[typeKey];
+      action[typeKey] = options.format(actionName, typeValue);
+    });
 
-        return action;
+    return action;
 
-    /* Otherwise return a plain action type */
-    } else {
-        return actionName;
-    }
+  /* Otherwise return a plain action type */
+  } else {
+    return actionName;
+  }
 }
