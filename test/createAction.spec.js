88a7e0660083060b3d00cdcd236db584f928043c
Artem Zakharchenko <kettanaito@gmail.com>
2017-06-13 09:55:02 +0200

    Create action: Fix indentation

--- test/createAction.spec.js
+++ test/createAction.spec.js
@@ -2,21 +2,21 @@ import { expect } from 'chai';
 import { createAction } from '../src';
 
 describe('Create action', () => {
-    it('Can be imported/required', () => {
-        return expect(createAction).to.not.be.undefined;
-    });
+  it('Can be imported/required', () => {
+    return expect(createAction).to.not.be.undefined;
+  });
 
-    /**
-     * When creating a new action using {actionCreate}, it automatically propagates it with
-     * the specified action types (i.e. request, success, error).
-     */
-    it('Creates action types properly', () => {
-        const myAction = createAction('GET_AUTHOR');
+  /**
+   * When creating a new action using {actionCreate}, it automatically propagates it with
+   * the specified action types (i.e. request, success, error).
+   */
+  it('Creates action types properly', () => {
+    const myAction = createAction('GET_AUTHOR');
 
-        return (
-            expect(myAction).to.have.property('request', 'GET_AUTHOR_REQUEST') &&
-            expect(myAction).to.have.property('success', 'GET_AUTHOR_SUCCESS') &&
-            expect(myAction).to.have.property('error', 'GET_AUTHOR_ERROR')
-        );
-    });
+    return (
+      expect(myAction).to.have.property('request', 'GET_AUTHOR_REQUEST') &&
+      expect(myAction).to.have.property('success', 'GET_AUTHOR_SUCCESS') &&
+      expect(myAction).to.have.property('error', 'GET_AUTHOR_ERROR')
+    );
+  });
 });
